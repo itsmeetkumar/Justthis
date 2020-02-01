896678f2009daf8900acdbc21b37cafeb49e0688
gaurip21 <60535304+gaurip21@users.noreply.github.com>
2020-02-01 04:26:58 -0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,16 @@
 <?php
 include 'include/config.php';
 ?>
+<head>
+	<link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body>
 <!-- Horizontal Form -->
             <div class="card card-info">
               <div class="card-header">
@@ -40,3 +50,18 @@ include 'include/config.php';
               </form>
             </div>
             <!-- /.card -->
+</body>
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- bs-custom-file-input -->
+<script src="../../plugins/bs-custom-file-input/bs-custom-file-input.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+  bsCustomFileInput.init();
+});
+</script>
