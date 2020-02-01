b3a71318074f689054a2cd8041903a02155b0c95
gaurip21 <60535304+gaurip21@users.noreply.github.com>
2020-02-01 04:51:35 -0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -33,12 +33,7 @@ include 'include/config.php';
           <div class="col-sm-6">
             <h1>General Form</h1>
           </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">General Form</li>
-            </ol>
-          </div>
+         
         </div>
       </div><!-- /.container-fluid -->
     </section>
